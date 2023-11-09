784c2ce283d745c05614cf676a1014553395df8e
OshidaBCF <swann.sanchez9@outlook.fr>
2023-11-09 12:19:38 +0100

    Ajoutez des fichiers projet.

---
+++ GameMaybe/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Laser.h"
+#include "Asteroide.h"
+#include "Ship.h"
+#include "Keyboard.h"
+
+class Game
+{
+	bool alive;
+	int score;
+	float dt;
+	float timeSinceLastAsteroide = 0;
+
+	std::vector<Laser> lasers;
+	std::vector<Asteroide> asteroides;
+	Ship ship;
+
+	Timer timer;
+	Keyboard keyboard;
+public:
+	void start();
+	void init();
+	void moveShip();
+	void addAsteroide();
+	void addLaser();
+	void moveObjects();
+	void checkLaserAstroColision();
+};
